87466b071da39625d6944bf8a661cea7dc394562
Amit Kumar <15000amitkumar@gmail.com>
2024-06-08 12:18:25 +0530

    product controller

---
+++ Question1/controllers/productController.js
@@ -0,0 +1,25 @@
+const { v4: uuidv4 } = require('uuid');
+const productService = require('../services/productService');
+
+const getProducts = async (req, res) => {
+    const { companyName, categoryName } = req.params;
+    const { top = 10, minPrice = 0, maxPrice = Infinity, page = 1 } = req.query;
+
+    try {
+        const products = await productService.getProducts(companyName, categoryName, top, minPrice, maxPrice);
+        const paginatedProducts = paginate(products, top, page);
+        const productsWithId = paginatedProducts.map(product => ({
+            ...product,
+            id: uuidv4(),
+        }));
+        res.json(productsWithId);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getProductById = async (req, res) => {
+    const { companyName, categoryName, productId } = req.params;
+
+    try {
+        const product = await product
