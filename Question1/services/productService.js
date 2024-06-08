3c489ff993522f0f9baa997c8c94dcc2063bd75e
Amit Kumar <15000amitkumar@gmail.com>
2024-06-08 12:09:22 +0530

    Product and auth service

--- Question1/services/productService.js
+++ Question1/services/productService.js
@@ -1,24 +1,60 @@
 const axios = require("axios");
+const authService = require("./authService");
 require("dotenv").config();
 
-const authService = {
-  getAuthToken: async () => {
-    try {
-      const response = await axios.post(process.env.AUTH_API_URL, {
-        companyName: process.env.COMPANY_NAME,
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        ownerName: process.env.OWNER_NAME,
-        ownerEmail: process.env.OWNER_EMAIL,
-        rollNo: process.env.ROLL_NO,
-      });
+const productService = {
+  getProducts: async (
+    company,
+    category,
+    top,
+    minPrice,
+    maxPrice,
+    page,
+    sort,
+    order
+  ) => {
+    const token = await authService.getAuthToken();
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+      params: {
+        top,
+        minPrice,
+        maxPrice,
+        page,
+        sort,
+        order,
+      },
+    };
 
-      return response.data.token;
+    try {
+      const response = await axios.get(
+        `${process.env.PRODUCTS_API_URL}/${company}/categories/${category}/products`,
+        config
+      );
+      return response.data;
     } catch (error) {
-      console.error("Error getting auth token:", error);
+      console.error("Error fetching products:", error);
+      throw error;
+    }
+  },
+
+  getProductDetails: async (company, category, productId) => {
+    const token = await authService.getAuthToken();
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+
+    try {
+      const response = await axios.get(
+        `${process.env.PRODUCTS_API_URL}/${company}/categories/${category}/products/${productId}`,
+        config
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching product details:", error);
       throw error;
     }
   },
 };
 
-module.exports = authService;
+module.exports = productService;
